c72a872ebc7ba8735eda61a173c2207ab8fc7e49
Kyle Shannon <kyle@pobox.com>
2017-07-05 09:10:32 -0600

    add debugging trace for pull

--- pool.go
+++ pool.go
@@ -159,10 +159,12 @@ func (p *notebookPool) newNotebook(image string, pull bool) (*tempNotebook, erro
 	// TODO(kyle): possibly provide tag support
 
 	if pull {
+		log.Printf("pulling container %s", image)
 		_, err = cli.ImagePull(ctx, image, types.ImagePullOptions{})
 		if err != nil {
 			return nil, err
 		}
+		log.Printf("successfully pulled")
 	}
 
 	hash := newHash(defaultHashSize)
