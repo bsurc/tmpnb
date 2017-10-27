f238f6b0b7a9530ce8d48a999513b4f803161b73
Kyle Shannon <kyle@pobox.com>
2017-10-27 16:03:36 -0600

    add more pool tests

--- pool_test.go
+++ pool_test.go
@@ -6,11 +6,18 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 	"regexp"
 	"testing"
 	"time"
 )
+
+func init() {
+	f, _ := os.Create(os.DevNull)
+	log.SetOutput(f)
+}
 
 func TestImageMatch(t *testing.T) {
 	match := regexp.MustCompile(jupyterNotebookImageMatch)
@@ -34,15 +41,18 @@ func TestImageMatch(t *testing.T) {
 	}
 }
 
+const skipDocker = "skipping docker dependent test"
+
 func TestNewNotebook(t *testing.T) {
 	if testing.Short() {
-		t.Skip("skipping docker dependent test")
+		t.Skip(skipDocker)
 	}
 	p, err := newNotebookPool(".*", 2, time.Minute*2)
 	if err != nil {
 		t.Fatal(err)
 	}
-	p.disableJupyterAuth = false
+	p.disableJupyterAuth = true
+	p.stopCollector()
 	nb, err := p.newNotebook("jupyter/minimal-notebook", false, "")
 	if err != nil {
 		t.Error(err)
@@ -54,4 +64,69 @@ func TestNewNotebook(t *testing.T) {
 	}
 	resp.Body.Close()
 	p.stopAndKillContainer(nb.id)
+}
+
+func TestCollection(t *testing.T) {
+	if testing.Short() {
+		t.Skip(skipDocker)
+	}
+	p, err := newNotebookPool(".*", 2, time.Second*5)
+	if err != nil {
+		t.Fatal(err)
+	}
+	p.disableJupyterAuth = true
+	// Stop the collector, then restart it with an aggressive rate
+	p.stopCollector()
+	p.startCollector(time.Second)
+	nb, err := p.newNotebook("jupyter/minimal-notebook", false, "")
+	if err != nil {
+		t.Error(err)
+	}
+	time.Sleep(time.Second * 6)
+	resp, err := http.Get(fmt.Sprintf("http://localhost:%d/?token=", nb.port))
+	if err == nil {
+		resp.Body.Close()
+		t.Error("container should be dead")
+	}
+	n := len(p.activeNotebooks())
+	if n != 0 {
+		t.Errorf("pool not drained (%d)", n)
+	}
+	p.stopAndKillContainer(nb.id)
+}
+
+func TestZombies(t *testing.T) {
+	if testing.Short() {
+		t.Skip(skipDocker)
+	}
+	p, err := newNotebookPool(".*", 2, time.Minute*2)
+	if err != nil {
+		t.Fatal(err)
+	}
+	p.disableJupyterAuth = true
+	p.stopCollector()
+	nb, err := p.newNotebook("jupyter/minimal-notebook", false, "")
+	if err != nil {
+		t.Error(err)
+	}
+	if len(p.containerMap) != 1 {
+		t.Error("failed to create container")
+	}
+	// manually remove the container from the container map, and drop the port
+	p.portSet.Drop(nb.port)
+	p.Lock()
+	delete(p.containerMap, nb.hash)
+	p.Unlock()
+	c, err := p.zombieContainers()
+	if len(c) != 1 {
+		t.Error("failed to locate zombie")
+	}
+	err = p.killZombieContainers()
+	if err != nil {
+		t.Error(err)
+	}
+	c, err = p.zombieContainers()
+	if len(c) != 0 {
+		t.Error("failed to kill zombie")
+	}
 }
